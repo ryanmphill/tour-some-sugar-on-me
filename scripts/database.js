27432a764c0e6f5e6c4325a29f36aa058d16fd28
Ryan Phillips <ryanmphill@gmail.com>
2023-05-03 16:10:12 -0500

    Added database object

---
+++ scripts/database.js
@@ -0,0 +1,199 @@
+const database = {
+    bands: [
+        {
+          id: 1,
+          name: "The Electric Bees",
+          numOfMembers: 6,
+          genre: "Indie Rock",
+          yearFormed: 2005
+        },
+        {
+          id: 2,
+          name: "Neon Skies",
+          numOfMembers: 4,
+          genre: "Synthpop",
+          yearFormed: 2018
+        },
+        {
+          id: 3,
+          name: "Crimson Horizon",
+          numOfMembers: 5,
+          genre: "Alternative Metal",
+          yearFormed: 2011
+        },
+        {
+          id: 4,
+          name: "Midnight Serenade",
+          numOfMembers: 3,
+          genre: "Jazz Fusion",
+          yearFormed: 2002
+        },
+        {
+          id: 5,
+          name: "Rising Tide",
+          numOfMembers: 7,
+          genre: "Pop Rock",
+          yearFormed: 2009
+        },
+        {
+          id: 6,
+          name: "Vortex Soundwave",
+          numOfMembers: 8,
+          genre: "Electronic",
+          yearFormed: 2017
+        },
+        {
+          id: 7,
+          name: "The Luminous Trees",
+          numOfMembers: 6,
+          genre: "Dream Pop",
+          yearFormed: 2014
+        },
+        {
+          id: 8,
+          name: "Echoing Embers",
+          numOfMembers: 2,
+          genre: "Folk",
+          yearFormed: 2008
+        },
+        {
+          id: 9,
+          name: "Solaris",
+          numOfMembers: 5,
+          genre: "Progressive Rock",
+          yearFormed: 2003
+        },
+        {
+          id: 10,
+          name: "The Velvet Whispers",
+          numOfMembers: 4,
+          genre: "R&B",
+          yearFormed: 2019
+        }
+      ],
+    venues: [
+        {
+          id: 1,
+          name: "The Grand Hall",
+          address: "123 Main Street",
+          squareFootage: 5100,
+          maxOccupancy: 141
+        },
+        {
+          id: 2,
+          name: "The Starlight Pavilion",
+          address: "456 Elm Street",
+          squareFootage: 6700,
+          maxOccupancy: 186
+        },
+        {
+          id: 3,
+          name: "The Crystal Ballroom",
+          address: "789 Oak Street",
+          squareFootage: 5700,
+          maxOccupancy: 158
+        },
+        {
+          id: 4,
+          name: "The Silver Lounge",
+          address: "321 Pine Street",
+          squareFootage: 3800,
+          maxOccupancy: 105
+        },
+        {
+          id: 5,
+          name: "The Sapphire Theater",
+          address: "654 Cedar Street",
+          squareFootage: 7200,
+          maxOccupancy: 200
+        },
+        {
+          id: 6,
+          name: "The Emerald Club",
+          address: "987 Maple Street",
+          squareFootage: 4200,
+          maxOccupancy: 116
+        },
+        {
+          id: 7,
+          name: "The Diamond Terrace",
+          address: "234 Birch Street",
+          squareFootage: 6100,
+          maxOccupancy: 169
+        },
+        {
+          id: 8,
+          name: "The Opal Lounge",
+          address: "567 Walnut Street",
+          squareFootage: 5000,
+          maxOccupancy: 138
+        }
+      ],
+    bookings: [
+        {
+            id: 1,
+            bandId: 2,
+            venueId: 8,
+            bookingDate: '5/21/2023'
+        },
+        {
+            id: 2,
+            bandId: 4,
+            venueId: 3,
+            bookingDate: '5/22/2023'
+        },
+        {
+            id: 3,
+            bandId: 7,
+            venueId: 8,
+            bookingDate: '5/24/2023'
+        },
+        {
+            id: 4,
+            bandId: 5,
+            venueId: 2,
+            bookingDate: '5/26/2023'
+        },
+        {
+            id: 5,
+            bandId: 3,
+            venueId: 7,
+            bookingDate: '5/28/2023'
+        },
+        {
+            id: 6,
+            bandId: 2,
+            venueId: 5,
+            bookingDate: '5/29/2023'
+        },
+        {
+            id: 7,
+            bandId: 9,
+            venueId: 1,
+            bookingDate: '6/1/2023'
+        },
+        {
+            id: 8,
+            bandId: 6,
+            venueId: 6,
+            bookingDate: '6/2/2023'
+        },
+        {
+            id: 9,
+            bandId: 7,
+            venueId: 2,
+            bookingDate: '6/3/2023'
+        },
+        {
+            id: 10,
+            bandId: 6,
+            venueId: 8,
+            bookingDate: '6/5/2023'
+        }
+    ]
+}
+
+
+  
+
+  
